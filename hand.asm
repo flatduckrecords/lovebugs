76186dabff2310672adbd6f78214a3b25da8da10
Robert Morrison <robert@flatduckrecords.co.uk>
2022-04-01 21:49:33 +0100

    add sprite data

---
+++ hand.asm
@@ -0,0 +1,20 @@
+; ASM source file created by SevenuP v1.20
+; SevenuP (C) Copyright 2002-2006 by Jaime Tejedor Gomez, aka Metalbrain
+
+;GRAPHIC DATA:
+;Pixel Size:      ( 16,  16)
+;Char Size:       (  2,   2)
+;Sort Priorities: X char, Char line, Y char, Mask
+;Data Outputted:  Gfx
+;Interleave:      Line
+;Mask:            Yes, before graphic
+
+hand:
+	DEFB	225,255,192,255,192,255,192, 63
+	DEFB	192,  7,192,  3,128,  0,  0,  0
+	DEFB	  0,  0,  0,  0,  0,  0,  0,  0
+	DEFB	  0,  0,128,  0,192,  0,224,  1
+	DEFB	 30,  0, 33,  0, 45,  0, 45,192
+	DEFB	 44, 56, 45,132,109,183,141,177
+	DEFB	175,181,175,245,191,253,191,253
+	DEFB	159,253, 79,253, 39,249, 16,  2
