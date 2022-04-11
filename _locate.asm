081aab6ee38d11c26aa98bd9bb905ced78e194b6
Robert Morrison <robert@flatduckrecords.co.uk>
2022-04-11 17:35:17 +0100

    modify LOCATE routines

--- _locate.asm
+++ _locate.asm
@@ -34,45 +34,57 @@ LOCATE:		LD	A,B
 ; Returns screen address in HL
 ;
 Get_Char_Address:       LD A,B
-                        AND %00000111
-                        RRA
-                        RRA
-                        RRA
-                        RRA
-                        OR C
-                        LD C,A
-                        LD A,B
-                        AND %00011000
-                        OR %01000000
-                        LD B,A
+			AND %00000111
+			RRA
+			RRA
+			RRA
+			RRA
+			OR C
+			LD C,A
+			LD A,B
+			AND %00011000
+			OR %01000000
+			LD B,A
 			LD HL,BC
-                        RET                             ; Returns screen address in HL
+			RET                             ; Returns screen address in HL
 
 
 ;;; Move HL down one pixel line
-Pixel_Address_Down:     INC H		; Go down onto the next pixel line
-                        LD A,H		; Check first three bits of high byte (Y0-Y2)
-                        AND 7
-                        RET NZ		; If any bits are set (we are inside a char line), we are done
+;Pixel_Address_Down:     INC H		; Go down onto the next pixel line
+;			LD A,H		; Check first three bits of high byte (Y0-Y2)
+;			AND 7
+;			RET NZ		; If any bits are set (we are inside a char line), we are done
 ;;; Move HL down one character line
 Next_Char_Line:
-                        LD A,L		; we crossed the char line ((Y0-Y2)=0 i.e Y6=1)
-                        ADD A,32	; (move down 1 char line) add the extra bit into (Y3-Y5)
-                        LD L,A
-                        RET NC		; Check for a carry bit (crossed a third), if C is set we are done
+			LD A,L		; we crossed the char line ((Y0-Y2)=0 i.e Y6=1)
+			ADD A,32	; (move down 1 char line) add the extra bit into (Y3-Y5)
+			LD L,A
+			RET NC		; Check for a carry bit (crossed a third), if C is set we are done
 ;crossed a third
-                        LD A,H		; increase bits Y6&7 (i.e. screen-third 0, 1 or 2);
-                        ADD %00001000	; 
+			LD A,H		; increase bits Y6&7 (i.e. screen-third 0, 1 or 2);
+			ADD %00001000	; 
 			AND %01011111
-                        LD H,A
-        
-	                RET		; we are done
+			LD H,A
+	
+			RET		; we are done
+Next_Char_Line_buff:
+			LD A,L		; we crossed the char line ((Y0-Y2)=0 i.e Y6=1)
+			ADD A,32	; (move down 1 char line) add the extra bit into (Y3-Y5)
+			LD L,A
+			RET NC		; Check for a carry bit (crossed a third), if C is set we are done
+;crossed a third
+			LD A,H		; increase bits Y6&7 (i.e. screen-third 0, 1 or 2);
+			ADD %00001000	; 
+			AND %11111111
+			LD H,A
+	
+			RET		; we are done
 
 ;move up one line
-Pixel_Address_Up:
-	ld a,h			;check if we’re within 7 bottom lines of a char row
-	and 7			;one of the three lowest bits will not be 0
-	jr nz,done		;if so, it is safe to decrement H and move one line up
+;Pixel_Address_Up:
+;	ld a,h			;check if we’re within 7 bottom lines of a char row
+;	and 7			;one of the three lowest bits will not be 0
+;	jr nz,done		;if so, it is safe to decrement H and move one line up
 Prev_Char_Line	
 	ld a,l			;else, load A with L
 	sub $20			;subtract 32 from it,
@@ -84,7 +96,6 @@ Prev_Char_Line
 	SUB %00001000	; 
 	ld h,a			;to move to the next char row
 
-done	;dec h			
-
+done	;dec h
 	ret
 
